c9a3e04f315da85b0189f44ed51a34a1049e815d
che.yanbing <cheyanbing@111.(none)>
2014-03-27 17:11:43 +0800

    Revert "Revert "Revert "update"""
    This reverts commit 3591984c6f9338749e02838bd8e411b48b22a1b1.

--- Client.php
+++
@@ -1,8 +0,0 @@
-<?php
-$client = new Yar_Client("http://127.0.0.1/api/");
-/* the following setopt is optinal */
-$client->SetOpt(YAR_OPT_CONNECT_TIMEOUT, 1);
-
-/* call remote service */
-$result = $client->api("parameter");
-?>
--- test.php
+++ test.php
@@ -1,6 +1,6 @@
-hello a hello
-hello bb hello
-hello  hello
-hello aabb hello
-hello  hello
-hello bb hello
+a
+aaa
+
+aa
+
+
